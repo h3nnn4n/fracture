fd6562a9c55f2bd31aa6f7f586794d85c5a4ec26
h3nnn4n <uber.renan@gmail.com>
2018-10-10 21:51:16 -0300

    Remove old code

--- main.js
+++ main.js
@@ -1,65 +1,12 @@
-/*jshint esversion: 6 */
-
 var manager;
-var background_color;
-var particles = [];
 var lines = [];
-
-var do_particles = false;
-var do_lines = true;
 
 function setup() {
   createCanvas(700, 600);
 
-  //colorMode(HSB);
-
-  if (do_particles) {
-    manager = new Manager();
-
-    manager.init(5);
-    //manager.init();
-
-    //var p = new Particle(
-      //0,
-      //height / 2,
-      //1
-    //);
-
-    //p.velocity.x = 1;
-    //p.velocity.y = 0;
-    //p.life = width;
-    //p.ninja = true;
-
-    //particles.push(p);
-  }
-
-  background_color = color(255);
-  background(background_color);
-
-  if (do_lines) {
-    lines.push(new Line());
-  }
+  background(color(255));
 
   draw_box();
-}
-
-function draw() {
-  if (do_particles) {
-    manager.update();
-
-    loadPixels();
-
-    for (var p in particles) {
-      particles[p].update();
-      particles[p].show();
-    }
-  }
-
-  if (do_lines) {
-    lines.push(new Line());
-  }
-
-  //console.log('loop');
 }
 
 function draw_box() {
@@ -70,4 +17,8 @@ function draw_box() {
   line(0, 0, width, 0);
   line(width, 0, width, height);
   line(0, height, width, height);
+}
+
+function draw() {
+  lines.push(new Line());
 }
