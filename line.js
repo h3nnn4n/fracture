17fc1d6fe90863bbe25866c5f1d9c589573feff9
h3nnn4n <uber.renan@gmail.com>
2018-10-10 21:48:53 -0300

    Use a simpler approach based on lines and algebra

---
+++ line.js
@@ -0,0 +1,137 @@
+/*jshint esversion: 6 */
+
+class Line {
+  constructor() {
+    this.linear_coef = random(-width / 2, width / 2);
+    this.angular_coef = randomGaussian(0, 2);
+
+    this.center = this.random_point();
+
+    this.draw_intersection(this.center, color(0, 0, 255));
+
+    this.start_point = createVector();
+    this.end_point = createVector();
+
+    this.bounds_start = createVector(-width / 2, -height / 2);
+    this.bounds_end = createVector(width / 2, height / 2);
+
+    this.set_full_bounds();
+
+    if (lines.length > 0) {
+      this.find_collisions();
+    }
+
+    this.render();
+  }
+
+  random_point() {
+    var x = random(-width / 2, width / 2);
+
+    return createVector(
+      x,
+      this.value_at_point(x)
+    );
+  }
+
+  value_at_point(x) {
+    return this.linear_coef + x * this.angular_coef;
+  }
+
+  set_full_bounds() {
+    console.log('setting full bonds');
+
+    this.start_point.x = this.bounds_start.x;
+    this.start_point.y = this.value_at_point(this.bounds_start.x);
+
+    this.end_point.x = this.bounds_end.x;
+    this.end_point.y = this.value_at_point(this.bounds_end.x);
+  }
+
+  find_collisions() {
+    for (var p in lines) {
+      var intersection = this.intersection_with(lines[p]);
+      this.draw_intersection(intersection);
+
+      if (this.start_point.x < intersection.x && intersection.x < this.center.x) {
+        if (lines[p].contains_point(intersection)) {
+          this.start_point.x = intersection.x;
+          this.start_point.y = intersection.y;
+        }
+      }
+
+      if (this.end_point.x > intersection.x && intersection.x > this.center.x) {
+        if (lines[p].contains_point(intersection)) {
+          this.end_point.x = intersection.x;
+          this.end_point.y = intersection.y;
+        }
+      }
+    }
+  }
+
+  contains_point(pos) {
+    return (this.start_point.x < pos.x && this.end_point.x > pos.x);
+  }
+
+  intersection_with(line) {
+    var x = (line.linear_coef - this.linear_coef) / (this.angular_coef - line.angular_coef);
+    var y = this.value_at_point(x);
+
+    return {x: x, y: y};
+  }
+
+  same(line) {
+    return (
+      Math.abs(this.angular_coef - line.angular_coef) < 1e-6 &&
+      Math.abs(this.linear_coef - line.linear_coef) < 1e-6
+    );
+  }
+
+  draw_intersection(pos, custom_color) {
+    return;
+
+    if (custom_color) {
+      stroke(custom_color);
+    } else {
+      stroke(color(0, 255, 0));
+    }
+
+    strokeWeight(1);
+
+    push();
+    translate(
+      width / 2,
+      height / 2
+    );
+
+    ellipse(
+      pos.x,
+      pos.y,
+      5
+    );
+
+    pop();
+  }
+
+  render() {
+    console.log(this.start_point);
+    console.log(this.end_point);
+
+    stroke(color(0, 0, 0));
+    strokeWeight(2);
+
+    push();
+    translate(
+      width / 2,
+      height / 2
+    );
+
+    line(
+      this.start_point.x,
+      this.start_point.y,
+      this.end_point.x,
+      this.end_point.y
+    );
+
+    pop();
+  }
+}
--- main.js
+++ main.js
@@ -3,43 +3,71 @@
 var manager;
 var background_color;
 var particles = [];
+var lines = [];
+
+var do_particles = false;
+var do_lines = true;
 
 function setup() {
   createCanvas(700, 600);
 
   //colorMode(HSB);
 
-  manager = new Manager();
+  if (do_particles) {
+    manager = new Manager();
 
-  manager.init(5);
-  //manager.init();
+    manager.init(5);
+    //manager.init();
 
-  //var p = new Particle(
-    //0,
-    //height / 2,
-    //1
-  //);
+    //var p = new Particle(
+      //0,
+      //height / 2,
+      //1
+    //);
 
-  //p.velocity.x = 1;
-  //p.velocity.y = 0;
-  //p.life = width;
-  //p.ninja = true;
+    //p.velocity.x = 1;
+    //p.velocity.y = 0;
+    //p.life = width;
+    //p.ninja = true;
 
-  //particles.push(p);
+    //particles.push(p);
+  }
 
   background_color = color(255);
   background(background_color);
+
+  if (do_lines) {
+    lines.push(new Line());
+  }
+
+  draw_box();
 }
 
 function draw() {
-  manager.update();
+  if (do_particles) {
+    manager.update();
 
-  loadPixels();
+    loadPixels();
 
-  for (var p in particles) {
-    particles[p].update();
-    particles[p].show();
+    for (var p in particles) {
+      particles[p].update();
+      particles[p].show();
+    }
+  }
+
+  if (do_lines) {
+    lines.push(new Line());
   }
 
   //console.log('loop');
+}
+
+function draw_box() {
+  stroke(color(0, 0, 0));
+  strokeWeight(4);
+
+  line(0, 0, 0, height);
+  line(0, 0, width, 0);
+  line(width, 0, width, height);
+  line(0, height, width, height);
 }
