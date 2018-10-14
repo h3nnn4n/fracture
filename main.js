504329e504253e46c8f427d21b2e1aaeff5871fd
h3nnn4n <uber.renan@gmail.com>
2018-10-14 20:48:05 -0300

    Spawn random lines as walls

--- main.js
+++ main.js
@@ -35,8 +35,8 @@ function spawn_line(parent) {
       x1: parent.end_point.x,
       y1: parent.end_point.y,
       length: Math.max(random(
-        parent.length + 5,
-        parent.length - 7.5
+        parent.length + 4.0,
+        parent.length - 6.0
       ), 5)
     })
   );
@@ -50,6 +50,14 @@ function reset() {
   background(color(255));
   lines = [];
   draw_box();
+
+  for (var i = 0; i < 3; i++) {
+    lines.push(
+      new Line({
+        active: false
+      })
+    );
+  }
 
   colorMode(HSB);
   for (var i = 0; i < 4; i++) {
